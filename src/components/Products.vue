2e501686822a4563fea73c5f50fa4c8fc568b921
nprofilovic <nprofilovic@gmail.com>
2019-11-26 13:24:29 +0100

    Initial files

---
+++ src/components/Products.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Products</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Products',
+  
+  
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Products />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Products from '@/components/Products.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Products
+  }
+}
+</script>
